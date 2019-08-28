94805fb96a0a8d9efecd92208da96355dc2e96ff
Jonathan Perry <jpper@umich.edu>
2019-08-28 23:08:38 +0900

    Implemented routing of the navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+//Components
+import Navbar from './components/Navbar';
 
 // Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={home}/>
-          <Route path="/login" component={login}/>
-          <Route path="/signup" component={signup}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route path="/" component={home} />
+                <Route path="/login" component={login} />
+                <Route path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
