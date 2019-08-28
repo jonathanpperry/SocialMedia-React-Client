5d83eecb5da2d648fd9eed5e4a9306bb04d22fb7
Jonathan Perry <jpper@umich.edu>
2019-08-29 08:46:14 +0900

    Completed login form implementation

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,21 +2,88 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = {
   form: {
     textAlign: 'center'
   },
-  image: '20px auto 20px auto'
+  image: {
+    margin: '20px auto 20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto'
+  },
+  textField: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10
+  },
+  progress: {
+    position: 'absolute'
+  }
 };
 
-export class login extends Component {
+class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {}
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios.post('/login', userData)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          loading: false
+        });
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        })
+      })
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -25,6 +92,25 @@ export class login extends Component {
           <Typography variant="h2" className={classes.pageTitle}>
             Login
           </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField id="email" name="email" type="email" label="Email" className={classes.classField} helperText={errors.email}
+              error={errors.email ? true : false} value={this.state.email} onChange={this.handleChange} fullWidth />
+            <TextField id="password" name="password" type="password" label="Password" className={classes.classField} helperText={errors.password}
+              error={errors.password ? true : false} value={this.state.password} onChange={this.handleChange} fullWidth />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button type="submit" variant="contained" color="primary" className={classes.button} disabled={loading}>
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>Don't have an account? Sign up <Link to="/signup">here</Link></small>
+          </form>
         </Grid>
         <Grid item sm />
       </Grid>
