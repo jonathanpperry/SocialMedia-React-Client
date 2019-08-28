632cc356745db0e6763d2f50262505ed3ece0a4a
Jonathan Perry <jpper@umich.edu>
2019-08-28 15:00:32 +0900

    First commit of social media React app!

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={home}/>
+          <Route path="/login" component={login}/>
+          <Route path="/signup" component={signup}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
