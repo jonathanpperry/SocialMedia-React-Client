592087fc2e22f015e46f825ccddeecd325e9498d
Jonny Ramen <jpper@umich.edu>
2019-08-29 14:47:00 +0900

    Finished signup and auth state section

--- src/pages/login.js
+++ src/pages/login.js
@@ -63,6 +63,7 @@ class login extends Component {
       .post("/login", userData)
       .then(res => {
         console.log(res.data);
+        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({
           loading: false
         });
