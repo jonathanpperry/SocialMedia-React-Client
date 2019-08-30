48036694628d3759076b39304caa535f88ba777d
Jonathan Perry <jpper@umich.edu>
2019-08-30 22:51:26 +0900

    Added in missing parenthese and reformatted

--- src/components/scream/Comments.js
+++ src/components/scream/Comments.js
@@ -24,43 +24,43 @@ class Comments extends Component {
         const { comments, classes } = this.props;
         return (
             <Grid container>
-                {comments.map(comment, index) => {
-                    const {body, createdAt, userImage, userHandle} = comment;
-    return (
+                {comments.map((comment, index) => {
+                    const { body, createdAt, userImage, userHandle } = comment;
+                    return (
                         <Fragment key={createdAt}>
-                    <Grid item sm={12}>
-                        <Grid container>
-                            <Grid item sm={2}>
-                                <img src={userImage} alt="comment" className={classes.commentImage} />
+                            <Grid item sm={12}>
+                                <Grid container>
+                                    <Grid item sm={2}>
+                                        <img src={userImage} alt="comment" className={classes.commentImage} />
+                                    </Grid>
+                                    <Grid item sm={9}>
+                                        <div className={classes.commentData}>
+                                            <Typography
+                                                variant="h5"
+                                                component={Link}
+                                                to={`/users/${userHangle}`}
+                                                color="primary">
+                                                {userHandle}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="textSecondary">
+                                                {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                                            </Typography>
+                                            <hr className={classes.invisibleSeparator} />
+                                            <Typography variant="body1">{body}</Typography>
+                                        </div>
+                                    </Grid>
+                                </Grid>
                             </Grid>
-                            <Grid item sm={9}>
-                                <div className={classes.commentData}>
-                                    <Typography
-                                        variant="h5"
-                                        component={Link}
-                                        to={`/users/${userHangle}`}
-                                        color="primary">
-                                        {userHandle}
-                                    </Typography>
-                                    <Typography
-                                        variant="body2"
-                                        color="textSecondary">
-                                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
-                                    </Typography>
-                                    <hr className={classes.invisibleSeparator} />
-                                    <Typography variant="body1">{body}</Typography>
-                                </div>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    {index !== comments.length - 1 && (
-                        <hr className={visibleSeparator} />
-                    )}
-                </Fragment>
-                )
-            }}
+                            {index !== comments.length - 1 && (
+                                <hr className={visibleSeparator} />
+                            )}
+                        </Fragment>
+                    );
+                })}
             </Grid>
-        )
+        );
     }
 }
 
