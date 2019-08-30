95e994f9d6717786461cc94ea7b883ad384a0e61
Jonathan Perry <jpper@umich.edu>
2019-08-31 01:13:37 +0900

    Initialized firebase, ran build and deployed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import themeFile from "./util/theme";
-import jwtDecode from "jwt-decode";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
 
 // Redux
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { SET_AUTHENTICATED } from "./redux/types";
-import { logoutUser, getUserData } from "./redux/actions/userActions";
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 //Components
-import Navbar from "./components/layout/Navbar";
-import AuthRoute from "./util/AuthRoute";
+import Navbar from './components/layout/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 // Pages
-import home from "./pages/home";
-import login from "./pages/login";
-import signup from "./pages/signup";
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 import user from './pages/user';
-import axios from "axios";
+import axios from 'axios';
 
 const theme = createMuiTheme(themeFile);
+
+axios.defaults.baseURL = 'https://europe-west1-socialape-710af.cloudfunctions.net/api';
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   store.dispatch(logoutUser());
   if (decodedToken.exp * 1000 < Date.now()) {
-    window.location.href = "/login";
+    window.location.href = '/login';
   } else {
     store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common['Authorization'] = token;
     store.dispatch(getUserData());
   }
 }
@@ -43,13 +46,13 @@ class App extends Component {
         <Provider store={store}>
           <Router>
             <Navbar />
-            <div className="container">
+            <div className='container'>
               <Switch>
-                <Route exact path="/" component={home} />
-                <AuthRoute exact path="/login" component={login} />
-                <AuthRoute exact path="/signup" component={signup} />
-                <Route exact path="/users/:handle" component={user} />
-                <Route exact path="/users/:handle/scream/:screamId" component={user} />
+                <Route exact path='/' component={home} />
+                <AuthRoute exact path='/login' component={login} />
+                <AuthRoute exact path='/signup' component={signup} />
+                <Route exact path='/users/:handle' component={user} />
+                <Route exact path='/users/:handle/scream/:screamId' component={user} />
               </Switch>
             </div>
           </Router>
