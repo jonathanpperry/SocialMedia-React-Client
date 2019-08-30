b3e66064f9e7ba5f384116538412cb5b7884e8f5
Jonathan Perry <jpper@umich.edu>
2019-08-30 22:13:20 +0900

    Implemented dialog details

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
 import DeleteScream from './DeleteScream';
 import ScreamDialog from './ScreamDialog';
+import LikeButton from './LikeButton';
 // MUI Stuff
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -15,12 +16,9 @@ import Typography from "@material-ui/core/Typography";
 
 // Icons
 import ChatIcon from '@material-ui/icons/Chat';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 // Redux
 import { connect } from 'react-redux';
-import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 
 const styles = {
   card: {
@@ -38,18 +36,6 @@ const styles = {
 };
 
 class Scream extends Component {
-  likedScream = () => {
-    if (this.props.user.likes && this.props.user.likes.find((like) => like.screamId === this.props.scream.screamId))
-      return true;
-    else return false;
-  };
-  likeScream = () => {
-    this.props.likeScream(this.props.scream.screamId);
-  }
-
-  unlikeScream = () => {
-    this.props.likeScream(this.props.scream.screamId);
-  }
 
   render() {
     dayjs.extend(relativeTime);
@@ -70,23 +56,6 @@ class Scream extends Component {
       }
     } = this.props;
 
-    const likeButton = !authenticated ? (
-      <MyButton tip="Like">
-        <Link to="/login">
-          <FavoriteBorder color="primary" />
-        </Link>
-      </MyButton>
-    ) : (
-        this.likedScream() ? (
-          <MyButton tip="Undo Like" onClick={this.unlikeScream}>
-            <FavoriteIcon color="primary" />
-          </MyButton>
-        ) : (
-            <MyButton tip="Like" onClick={this.likeScream}>
-              <FavoriteBorder color="primary" />
-            </MyButton>
-          )
-      );
     const deleteButton = authenticated && userHandle === handle ? (
       <DeleteScream screamId={screamId} />
     ) : null
@@ -111,7 +80,7 @@ class Scream extends Component {
             {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
-          {likeButton}
+          <LikeButton screamId={screamId}/>
           <span>{likeCount} Likes</span>
           <MyButton tip="comments">
             <ChatIcon color="primary" />
@@ -125,8 +94,6 @@ class Scream extends Component {
 }
 
 Scream.propTypes = {
-  likeScream: PropTypes.func.isRequired,
-  unlikeScream: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -2,18 +2,19 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from '../util/MyButton';
+import LikeButton from './LikeButton';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom'
 // MUI
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 // Icons
 import CloseIcon from '@material-ui/icons/Close';
 import UnfoldMore from '@material-ui/icons/UnfoldMore';
+import ChatIcon from '@material-ui/icons/Chat';
 // Redux
 import { connect } from 'react-redux';
 import { getScream } from '../redux/actions/dataActions';
@@ -36,6 +37,15 @@ const styles = theme => ({
     closeButton: {
         position: 'absolute',
         left: '90%'
+    },
+    expandButton: {
+        position: 'absolute',
+        left: '90%'
+    },
+    spinnerDiv: {
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 50
     }
 });
 
@@ -61,7 +71,9 @@ class ScreamDialog extends Component {
             UI: { loading }
         } = this.props;
         const dialogMarkup = loading ? (
-            <CircularProgress size={200} />
+            <div className={classes.spinnerDiv}>
+                <CircularProgress size={200} thickness={2} />
+            </div>
         ) : (
                 <Grid container spacing={16}>
                     <Grid item sm={5}>
@@ -84,6 +96,12 @@ class ScreamDialog extends Component {
                         <Typography variant="body1">
                             {body}
                         </Typography>
+                        <LikeButton screamId={screamId} />
+                        <span>{likeCount} likes</span>
+                        <MyButton tip="comments">
+                            <ChatIcon color="primary" />
+                        </MyButton>
+                        <span>{commentCount} Comments:</span>
                     </Grid>
 
                 </Grid>
